180aed9158b1b517f673ce49c4f55addd5c90a1f
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-03-26 16:41:01 +0600

    favicon and logo change

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,8 +10,10 @@ const Navbar = () => {
     })
   return (
     <div className='bg-white/20 w-full backdrop-blur-md fixed top-0 left-0 right-0 p-3 grid grid-cols-10'>
-        <p className="col-span-5 text-xl font-semibold">Muztahid Rahman</p>
-        <div className="col-span-5 flex items-center justify-end gap-x-5">
+        <div className="col-span-3">
+          <img src='/logo.png' className='h-[30px]'/>
+        </div>
+        <div className="col-span-7 flex items-center justify-end gap-x-5">
             <a href='#' onClick={()=> window&&gsap.to(window, { duration: 1, scrollTo: { y: "#projects", offsetY: 50 },ease:'expo.inOut'} )} className='text-sm hidden md:block'>Projects</a>
             <a href="#" onClick={()=> window&&gsap.to(window, { duration: 2, scrollTo: { y: "#skills", offsetY: 50 },ease:'expo.inOut'} )} className='text-sm hidden md:block'>Skills</a>
             <Button onClick={()=> window&&gsap.to(window, { duration: 2, scrollTo: { y: "#contact", offsetY: 50 },ease:'expo.inOut'} )} className='ml-3'>Contact</Button>
