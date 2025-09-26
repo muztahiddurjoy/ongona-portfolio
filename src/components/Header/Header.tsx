9eb4f9f3a4191f95d15e4da9399e83a3ad5bdef1
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2025-09-26 17:20:08 +0600

    update Header component to include LinkedIn button and fix Ongona link; update project links in Projects component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 
-import { Github } from 'lucide-react'
+import { Github, Linkedin } from 'lucide-react'
 import { Button } from '../ui/button'
 import HeaderSVG from './SVG'
 
@@ -7,11 +7,12 @@ const Header = () => {
   return (
     <div className='md:h-[100vh] flex items-center flex-col-reverse md:flex-row md:px-20 px-5'>
         <div className="flex-1 flex justify-center-center flex-col">
-            <p className='text-sm'>Co-founder @ <a href='https://ongona.app' className='font-medium text-theme2'>Ongona</a></p>
+            <p className='text-sm'>Co-founder @ <a href='https://ongona.com' className='font-medium text-theme2'>Ongona</a></p>
             <h1 className="text-4xl md:text-5xl font-extrabold mt-3 gdText">Muztahid Rahman</h1>
             <p className='mt-5 text-sm text-gray-700'>Hi! I'm Muztahid Rahman. I love to do coding and create something new.</p>
             <div className="flex">
                 <a href="https://github.com/muztahiddurjoy" target='_blank'><Button className='mt-3'><Github height={15} width={15}/> <span className='ml-1'>Github</span></Button></a>
+                <a href="https://www.linkedin.com/in/muztahid-rahman" target='_blank'><Button className='mt-3 ml-3' variant='default'><Linkedin size={15} className='mr-1 fill-white'/> LinkedIn</Button></a>
             </div>
         </div>
         <div className="flex-1 mt-20 md:mt-0">
